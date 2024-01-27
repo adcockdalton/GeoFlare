85fcb301e8e71897fc735f50523bef990def56cc
caosl <caosl@uci.edu>
2024-01-27 00:25:50 -0800

    get button to the home landing page for access to upload page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import Link from "next/link";
+import { cn } from "@/lib/utils"
 import { Home, Map } from "lucide-react";
 
 const inter = Inter({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import Link from "next/link"
-
+import { ArrowUpRightIcon } from "lucide-react"
 import { Button } from "@/components/ui/button"
 
 
 export default function Home() {
   return (
-    <main className="flex h-full justify-center relative  bg-geo-grey"></main>
+    <div>
+      <main className="flex h-full justify-center relative  bg-geo-grey"></main>
+      
+      <Link href="/upload">
+        <Button className="flex gap-2 p-6 text-lg font-bold text-white bg-orange-500 rounded-full w-fit dark:text-black">
+          <p>Get Started</p> <ArrowUpRightIcon />
+        </Button>
+      </Link>
+  </div>
+
   );
 }
