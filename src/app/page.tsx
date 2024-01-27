f246e97251fa517f1255213f17e34f35f6c02b91
TimTimich6 <timofeyobraztsov@gmail.com>
2024-01-27 01:09:39 -0800

    style: main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,12 @@ import { ArrowUpRightIcon } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>
-      <main className="flex h-full justify-center relative  bg-geo-grey"></main>
+    <main className="flex h-full justify-center relative  bg-geo-grey">
       <Link href="/upload">
         <Button className="flex gap-2 p-6 text-lg font-bold text-white bg-orange-500 rounded-full w-fit dark:text-black">
           <p>Get Started</p> <ArrowUpRightIcon />
         </Button>
       </Link>
-    </div>
+    </main>
   );
 }
