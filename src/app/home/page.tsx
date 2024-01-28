b68fead2f734aaab7d4a5173c3fd0542dc6651f1
Jasmine <jasmineqw45@gmail.com>
2024-01-27 18:41:28 -0800

    updated ui for location page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -8,6 +8,9 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { AlertTriangle, Clock, HomeIcon } from "lucide-react";
+import { ArrowUpRightIcon } from "lucide-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faClock, faHouse, faTriangleExclamation} from "@fortawesome/free-solid-svg-icons"
 
 function Home() {
   const router = useRouter();
@@ -17,24 +20,26 @@ function Home() {
   };
 
   return (
-    <div className="flex flex-col  bg-geo-dark px-8 pb-8  rounded-xl  pt-16 overflow-y-auto h-full items-center ">
+    <div className="flex flex-col  bg-geo-dark px-8 pb-8  rounded-xl  pt-16 overflow-y-auto h-full items-center " 
+    style={{backgroundColor: '#14171A'}}>
       <div className=" flex flex-col w-3/5 gap-8 relative">
         <div className="flex justify-center">
-          <h1 className="text-4xl font-medium text-center text-white">
+          <h1 className="text-6xl font-medium text-center text-white">
             Enter a location. Watch the <br />
             magic happen
           </h1>
         </div>
-        <div className="flex items-center justify-between gap-2 w-full">
+        <div className="flex items-center justify-between gap-6 w-full">
           <Input
             type="input"
             placeholder="enter a location"
-            className="bg-geo-grey border-0 outline-none text-white text-xl py-8 px-6 placeholder:text-slate-500"
+            className=" border-0 outline-none text-white text-xl py-10 px-6 placeholder:text-{rgba(255, 255, 255, .25)}"
+            style={{backgroundColor: '#1A1E20'}}
           />
           <Button className="bg-geo-teal h-full text-lg">Create Map</Button>
         </div>
 
-        <Card className=" bg-geo-dark border-geo-grey ">
+        <Card className= 'rounded-xl' style={{backgroundColor:'#14171A', border: '3px solid rgba(255, 255, 255, .1)'}}>
           <CardHeader>
             <CardTitle className="text-white text-2xl font-medium">
               Critical locations near you
@@ -42,32 +47,35 @@ function Home() {
           </CardHeader>
           <CardContent className="flex items-center justify-between  ">
             <Card
-              className=" flex w-full items-center relative  bg-geo-someotherfuckignshadeofgrey cursor-pointer hover:shadow-md border-geo-grey border-2 p-6 gap-4  "
+              className=" flex w-full items-center relative cursor-pointer hover:shadow-md border-geo-grey border-3 p-7 gap-8  "
+              style={{backgroundColor:'#1C2125', border: '3px solid rgba(255, 255, 255, .1)'}}
               //   onClick={() => handleRedirectClick("/unknown")}
             >
-              <div className="flex justify-between  h-full flex-col w-full gap-2 overflow-x-auto">
+              <div className="flex justify-between  h-full flex-col w-full gap-6 overflow-x-auto">
                 <CardTitle className="text-white">
                   Anteatery Hills, Irvine
                 </CardTitle>
 
                 <div className="flex justify-start gap-4 text-sm font-medium items-center w-full ">
-                  <Badge text="23 minutes">
-                    <Clock></Clock>
-                  </Badge>
-                  <Badge
-                    text="moderate risk"
-                    bg="bg-yellow-600"
-                    style="text-yellow-300"
-                  >
-                    <AlertTriangle fill="orange"></AlertTriangle>
-                  </Badge>
-                  <Badge text="28 homes at risk" bg="" style="">
-                    <HomeIcon fill="orange"></HomeIcon>
-                  </Badge>
+                  <div className="tag" style={{backgroundColor: '#3C3828', color: '#EEBB37'}}>
+                  <FontAwesomeIcon icon={faTriangleExclamation} />
+                      Moderate risk
+                  </div>
+                  <div className="tag" style={{backgroundColor: '', color: '#fff'}}>
+                  <FontAwesomeIcon icon={faClock} />
+                     23 minutes
+                  </div>
+                  <div className="tag" style={{backgroundColor: '', color: '#fff'}}>
+                  <FontAwesomeIcon icon={faHouse} />
+                     28 homes at risk
+                  </div>
+               
+                
                 </div>
               </div>
               <Link href="/map">
-              <Button className="bg-geo-light  text-lg">Launch</Button>
+              <Button className="bg-geo-light  text-lg p-7">Launch
+              <ArrowUpRightIcon /></Button>
               </Link>
             </Card>
           </CardContent>
