69116fc0b6870e6d379b9c3d262f8d93849925a5
TimTimich6 <timofeyobraztsov@gmail.com>
2024-01-27 21:55:42 -0800

    dynamic sidebar

---
+++ src/components/sidebar-button.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+  src: string;
+  url: string;
+  selected: boolean;
+}
+function SidebarButton(props: Props) {
+  return (
+    <Link href={"/" + props.url}>
+      <Image
+        priority
+        width={50}
+        height={50}
+        className={
+          "relative p-2 rounded-lg  aspect-square " +
+          (props.selected ? "bg-geo-teal" : null)
+        }
+        src={props.src}
+      ></Image>
+    </Link>
+  );
+}
+
+export default SidebarButton;
