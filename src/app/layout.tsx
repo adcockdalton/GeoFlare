22eed2df078d4007cee2ece9490de056612aad6a
caosl <caosl@uci.edu>
2024-06-08 14:04:14 -0700

    fix layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,22 @@ import "./globals.css";
 
 import Wrapper from "./wrapper";
 
+/**
+ * Represents the Alliance font used in the application layout.
+ *
+ * @typedef {Object} AllianceFont
+ * @property {Array<{ path: string, weight: string, style: string }>} src - The array of font sources.
+ * @property {string} display - The CSS value for the font display property.
+ * @property {string} variable - The CSS variable name for the font.
+ * @property {boolean} preload - Indicates whether the font should be preloaded.
+ */
+
+/**
+ * Creates an instance of the Alliance font.
+ *
+ * @param {AllianceFont} options - The options for creating the Alliance font.
+ * @returns {void}
+ */
 const alliance = localFont({
   src: [
     { path: "/font/AllianceNo2-Regular.ttf", weight: "400", style: "normal" },
@@ -18,11 +34,22 @@ const alliance = localFont({
   preload: true,
 });
 
+
+
 export const metadata: Metadata = {
   title: "GeoFlare",
   description: "Wildfire fighting tool powered by AI.",
 };
 
+
+
+
+/**
+ * Renders the root layout of the application.
+ *
+ * @param children - The child components to be rendered within the layout.
+ * @returns The JSX element representing the root layout.
+ */
 export default function RootLayout({
   children,
 }: {
